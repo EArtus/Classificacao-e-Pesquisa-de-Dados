7d5d865d9d25483489da9c4260b9929faf4b547e
EArtus <eartus1112@gmail.com>
2024-09-16 16:53:22 -0300

    ShellSort

---
+++ ShellSort.py
@@ -0,0 +1,17 @@
+def shell_sort(arr):
+    gap = len(arr) // 2
+
+    while gap > 0:
+        for i in range(gap, len(arr)):
+            temp = arr[i]
+            j = i
+            while j >= gap and arr[j - gap] > temp:
+                arr[j] = arr[j - gap]
+                j -= gap
+            arr[j] = temp
+        
+        gap //= 2
+
+arr = [12, 34, 54, 2, 3, 21]
+shell_sort(arr)
+print(arr)
